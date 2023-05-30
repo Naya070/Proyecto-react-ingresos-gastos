44c6d6519dc6e3ac676a20645a2157adc92a6693
Naya070 <nayarith070@gmail.com>
2023-05-30 13:36:20 -0400

    se agrego libreria de iconos

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,10 @@ function App() {
   return (
     <GlobalProvider>
       <div className="bg-neutral-950 text-white  flex justify-center items-center w-full">
-        <div className="w-2/5 flex justify-center items-center sm:w-4/5 ">
+        <div className="w-2/5 justify-center items-center sm:w-4/5 ">
+        <Header />
           <div className="bg-neutral-800 justify-center p-10 rounded-md w-full">
-            <Header />
+            
             <div className="flex flex-col sm:flex-row justify-between gap-4">
               <div className="flex-1 items-center justify-center">
                 <div className="mb-20">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
+import { AiFillSetting } from "react-icons/ai";
+import { MdWorkHistory } from "react-icons/md";
+
+
 export function Header() {
     return (
-        <div className="text-4xl font-bold text-center my-5">
-            Expense Tracker
+        <div className="flex flex-row justify-around bg-zinc-600 
+            text-2lg font-bold text-center w-full my-5 rounded-md">
+            <div className="flex my-5 justify-between "><MdWorkHistory/>History</div>
+            <div className=" bg-red-600 text-lg font-bold text-normal my-5">
+                Expense Tracker
+            </div>
+            <div className="my-5"><AiFillSetting/></div>
+            
         </div>
     )
 }
