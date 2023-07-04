f81e9e1f963e68848133520fda9a1d701591bdb1
Naya070 <nayarith070@gmail.com>
2023-07-03 20:07:16 -0400

    Prueba deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,15 @@ import { MdWorkHistory } from "react-icons/md";
 export function Header() {
     return (
         <div className="flex flex-row justify-around bg-zinc-600 
-            text-2lg font-bold text-center w-full my-5 rounded-md">
-            <div className="flex my-5 justify-between "><MdWorkHistory/>History</div>
-            <div className=" bg-red-600 text-lg font-bold text-normal my-5">
-                Expense Tracker
+            text-lg font-bold text-center w-full my-5 rounded-md">
+            <div className="flex my-5 justify-between ">
+                <div className="text-2xl"><MdWorkHistory/></div>
+                History
             </div>
-            <div className="my-5"><AiFillSetting/></div>
+            <div className="text-lg font-bold text-normal my-5">
+                Accounting Record App
+            </div>
+            <div className="my-5 text-2xl "><AiFillSetting/></div>
             
         </div>
     )
